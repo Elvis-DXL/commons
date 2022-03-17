a8775677fdf24e06e7f6e43fbac691f1cf57ae94
Elvis-DXL <dxiaolong@outlook.com>
2022-03-17 12:00:46 +0800

    初次提交

--- src/main/java/com/elvis/commons/utils/ExcelUtil.java
+++ src/main/java/com/elvis/commons/utils/ExcelUtil.java
@@ -1,11 +1,14 @@
 package com.elvis.commons.utils;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import org.apache.poi.ss.usermodel.Workbook;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 
 /**
  * @author : Elvis
@@ -16,7 +19,7 @@ public final class ExcelUtil {
     private ExcelUtil() {
     }
 
-    public static void exportExcel(HSSFWorkbook wb, String fileName,
+    public static void exportExcel(Workbook wb, String fileName,
                                    HttpServletRequest request, HttpServletResponse response) {
         fileName = fileName + ".xls";
         fileName = DownloadUtil.encodeDownloadFileName(fileName, request);
@@ -35,5 +38,12 @@ public final class ExcelUtil {
         }
     }
 
+    public static void exportExcel(List<?> dataList, Class<?> pojoClass, String title, String sheetName, String fileName,
+                                   HttpServletResponse response, HttpServletRequest request) {
+        Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams(title, sheetName), pojoClass, dataList);
+        if (null != workbook) {
+            exportExcel(workbook, fileName, request, response);
+        }
+    }
 
 }
