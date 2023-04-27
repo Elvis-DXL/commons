f224c3ea72de04d02b0df40678461d750e1e43de
daixiaolong <Elvis216>
2023-04-27 11:34:07 +0800

    初次提交

--- src/main/java/com/elvis/commons/utils/IdCardUtil.java
+++ src/main/java/com/elvis/commons/utils/IdCardUtil.java
@@ -12,37 +12,39 @@ import java.util.Date;
  * @since : 2021/11/24 16:52
  */
 public final class IdCardUtil {
+
     private IdCardUtil() {
+        throw new AssertionError("Utility classes do not allow instantiation");
     }
 
-    public static Date getBirthday(String idCardNumber) {
-        return DateUtil.parseDate(idCardNumber.substring(6, 14), DPEnum.yyyyMMdd);
+    public static Date birthday(String idCard) {
+        return DateUtil.parseDate(idCard.substring(6, 14), DPEnum.yyyyMMdd);
     }
 
-    public static String getBirthdayStr(String idCardNumber, String pattern) {
-        return DateUtil.formatDate(getBirthday(idCardNumber), pattern);
+    public static String birthdayStr(String idCard, String pattern) {
+        return DateUtil.formatDate(birthday(idCard), pattern);
     }
 
-    public static String getBirthdayStr(String idCardNumber, DPEnum dpEnum) {
-        return getBirthdayStr(idCardNumber, dpEnum.pattern());
+    public static String birthdayStr(String idCard, DPEnum dpEnum) {
+        return birthdayStr(idCard, dpEnum.pattern());
     }
 
-    public static int currAge(String idCardNumber) {
-        return dateAge(idCardNumber, new Date());
+    public static int currAge(String idCard) {
+        return dateAge(idCard, new Date());
     }
 
-    public static int dateAge(String idCardNumber, Date date) {
-        int bornYear = Integer.parseInt(idCardNumber.substring(6, 10));
+    public static int dateAge(String idCard, Date date) {
+        int bornYear = Integer.parseInt(idCard.substring(6, 10));
         int dateYear = DateUtil.get(date, Calendar.YEAR);
         return Math.max(dateYear - bornYear, 0);
     }
 
-    public static int sex(String idCardNumber) {
-        int sex = Integer.parseInt(idCardNumber.substring(16, 17));
+    public static int sex(String idCard) {
+        int sex = Integer.parseInt(idCard.substring(16, 17));
         return sex % 2;
     }
 
-    public static String sexStr(String idCardNumber) {
-        return sex(idCardNumber) == 1 ? "男" : "女";
+    public static String sexStr(String idCard) {
+        return sex(idCard) == 1 ? "男" : "女";
     }
 }
