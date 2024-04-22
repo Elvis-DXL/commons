4e54c50824e57ae7a14a95564c722f7877bc8453
daixiaolong <982424518@qq.com>
2024-04-22 16:48:39 +0800

    初始化工具

--- src/main/java/com/elvis/commons/temp/DSUtil.java
+++ src/main/java/com/elvis/commons/temp/DSUtil.java
@@ -1,148 +1,279 @@
 package com.elvis.commons.temp;
 
+import java.lang.reflect.Field;
 import java.text.DecimalFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
+/**
+ * @Author : 慕君Dxl
+ * @CreateTime : 2024/4/19 16:45
+ */
 public final class DSUtil {
+    //私有化构造器，禁止创建对象
     private DSUtil() {
+        throw new AssertionError("Utility classes do not allow instantiation");
     }
 
-    //时间日期格式
-    public static String yyyy_MM_dd_HH_mm_ss_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
-    public static String yyyy_MM_dd_HH_mm_ss = "yyyy-MM-dd HH:mm:ss";
-    public static String yyyy_MM_dd_HH_mm = "yyyy-MM-dd HH:mm";
-    public static String yyyy_MM_dd_HH = "yyyy-MM-dd HH";
-    public static String yyyy_MM_dd = "yyyy-MM-dd";
-    public static String yyyy_MM = "yyyy-MM";
-    public static String HH_mm_ss = "HH:mm:ss";
-    public static String HH_mm = "HH:mm";
-    public static String yyyyMMddHHmmssSSS = "yyyyMMddHHmmssSSS";
-    public static String yyyyMMddHHmmss = "yyyyMMddHHmmss";
-    public static String yyyyMMddHHmm = "yyyyMMddHHmm";
-    public static String yyyyMMddHH = "yyyyMMddHH";
-    public static String yyyyMMdd = "yyyyMMdd";
-    public static String yyyyMM = "yyyyMM";
-    public static String HHmmssSSS = "HHmmssSSS";
-    public static String HHmmss = "HHmmss";
-    public static String HHmm = "HHmm";
-    public static String c_yyyy_MM_dd_HH_mm_ss_SSS = "yyyy年MM月dd日HH时mm分ss秒SSS毫秒";
-    public static String c_yyyy_MM_dd_HH_mm_ss = "yyyy年MM月dd日HH时mm分ss秒";
-    public static String c_yyyy_MM_dd_HH_mm = "yyyy年MM月dd日HH时mm分";
-    public static String c_yyyy_MM_dd_HH = "yyyy年MM月dd日HH时";
-    public static String c_yyyy_MM_dd = "yyyy年MM月dd日";
-    public static String c_yyyy_MM = "yyyy年MM月";
-    public static String c_yyyy = "yyyy年";
-    public static String c_HH_mm_ss_SSS = "HH时mm分ss秒SSS毫秒";
-    public static String c_HH_mm_ss = "HH时mm分ss秒";
-    public static String c_HH_mm = "HH时mm分";
-    public static String c_yyyy_MM_dd_EE = "yyyy年MM月dd日 EE";
-    public static String c_EE = "EE";
-    public static String x_yyyy_MM_dd_HH_mm_ss_SSS = "yyyy/MM/dd HH:mm:ss.SSS";
-    public static String x_yyyy_MM_dd_HH_mm_ss = "yyyy/MM/dd HH:mm:ss";
-    public static String x_yyyy_MM_dd_HH_mm = "yyyy/MM/dd HH:mm";
-    public static String x_yyyy_MM_dd_HH = "yyyy/MM/dd HH";
-    public static String x_yyyy_MM_dd = "yyyy/MM/dd";
-    public static String x_yyyy_MM = "yyyy/MM";
-    public static String xh_yyyy_MM_dd_HH = "yyyy_MM_dd_HH";
-    public static String xh_yyyy_MM_dd = "yyyy_MM_dd";
-    public static String xh_yyyy_MM = "yyyy_MM";
-    public static String yyyy = "yyyy";
-    public static String MM = "MM";
-    public static String dd = "dd";
-    public static String HH = "HH";
-    public static String mm = "mm";
-    public static String ss = "ss";
-    public static String SSS = "SSS";
+    /****************************************以上为私有构造器，以下为常量****************************************/
+    //地球半径,单位:m
+    public static final double EARTH_RADIUS = 6371393;
 
-    //地球平均半径(m)
-    private static final double EARTH_RADIUS = 6371393;
+    //零到九字符串集合
+    public static final List<String> ZERO_TO_NINE = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
 
-    public static String formatDate(Date date, String pattern) {
-        return new SimpleDateFormat(pattern).format(date);
-    }
+    //时间格式化枚举
+    public enum DatePattern {
+        yyyy_MM_dd_HH_mm_ss_SSS("yyyy-MM-dd HH:mm:ss.SSS"),
+        yyyy_MM_dd_HH_mm_ss("yyyy-MM-dd HH:mm:ss"),
+        yyyy_MM_dd_HH_mm("yyyy-MM-dd HH:mm"),
+        yyyy_MM_dd_HH("yyyy-MM-dd HH"),
+        yyyy_MM_dd("yyyy-MM-dd"),
+        yyyy_MM("yyyy-MM"),
+        HH_mm_ss("HH:mm:ss"),
+        HH_mm("HH:mm"),
+        yyyyMMddHHmmssSSS("yyyyMMddHHmmssSSS"),
+        yyyyMMddHHmmss("yyyyMMddHHmmss"),
+        yyyyMMddHHmm("yyyyMMddHHmm"),
+        yyyyMMddHH("yyyyMMddHH"),
+        yyyyMMdd("yyyyMMdd"),
+        yyyyMM("yyyyMM"),
+        HHmmssSSS("HHmmssSSS"),
+        HHmmss("HHmmss"),
+        HHmm("HHmm"),
+        c_yyyy_MM_dd_HH_mm_ss_SSS("yyyy年MM月dd日HH时mm分ss秒SSS毫秒"),
+        c_yyyy_MM_dd_HH_mm_ss("yyyy年MM月dd日HH时mm分ss秒"),
+        c_yyyy_MM_dd_HH_mm("yyyy年MM月dd日HH时mm分"),
+        c_yyyy_MM_dd_HH("yyyy年MM月dd日HH时"),
+        c_yyyy_MM_dd("yyyy年MM月dd日"),
+        c_yyyy_MM("yyyy年MM月"),
+        c_yyyy("yyyy年"),
+        c_HH_mm_ss_SSS("HH时mm分ss秒SSS毫秒"),
+        c_HH_mm_ss("HH时mm分ss秒"),
+        c_HH_mm("HH时mm分"),
+        c_yyyy_MM_dd_EE("yyyy年MM月dd日 EE"),
+        x_yyyy_MM_dd_EE("yyyy/MM/dd EE"),
+        yyyy_MM_dd_EE("yyyy-MM-dd EE"),
+        c_EE("EE"),
+        x_yyyy_MM_dd_HH_mm_ss_SSS("yyyy/MM/dd HH:mm:ss.SSS"),
+        x_yyyy_MM_dd_HH_mm_ss("yyyy/MM/dd HH:mm:ss"),
+        x_yyyy_MM_dd_HH_mm("yyyy/MM/dd HH:mm"),
+        x_yyyy_MM_dd_HH("yyyy/MM/dd HH"),
+        x_yyyy_MM_dd("yyyy/MM/dd"),
+        x_yyyy_MM("yyyy/MM"),
+        xh_yyyy_MM_dd_HH("yyyy_MM_dd_HH"),
+        xh_yyyy_MM_dd("yyyy_MM_dd"),
+        xh_yyyy_MM("yyyy_MM"),
+        ;
+        private final String val;
 
-    public static Date parseDate(String dateStr, String pattern) {
-        Date date = null;
-        try {
-            date = new SimpleDateFormat(pattern).parse(dateStr);
-        } catch (ParseException e) {
-            e.printStackTrace();
+        DatePattern(String val) {
+            this.val = val;
         }
-        return date;
-    }
 
-    public static Calendar dateToCalendar(Date date) {
-        if (null == date) {
-            date = new Date();
+        public String val() {
+            return val;
         }
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        return calendar;
     }
 
-    public static Date addDate(Date date, int calendarType, int num) {
-        Calendar calendar = dateToCalendar(date);
-        calendar.add(calendarType, num);
-        return calendar.getTime();
+    //特殊符号枚举
+    public enum Symbol {
+        DH(","),
+        FH(";"),
+        XHX("_"),
+        ZHX("-"),
+        YWD("."),
+        BFH("%"),
+        MYF("$"),
+        RMB("￥"),
+        ADF("@"),
+        ZXX("/"),
+        ;
+        private final String val;
+
+        Symbol(String val) {
+            this.val = val;
+        }
+
+        public String val() {
+            return val;
+        }
+
+        public Boolean included(String aimStr) {
+            return null != aimStr && aimStr.length() != 0 && aimStr.trim().contains(this.val);
+        }
+
+        public Boolean notIncluded(String aimStr) {
+            return !this.included(aimStr);
+        }
+
+        public List<String> split(String aimStr) {
+            if (this.notIncluded(aimStr)) {
+                return new ArrayList<>();
+            }
+            StringTokenizer st = new StringTokenizer(aimStr, this.val);
+            List<String> result = new ArrayList<>();
+            while (st.hasMoreTokens()) {
+                result.add(st.nextToken());
+            }
+            return result;
+        }
+
+        public List<Long> splitToLong(String aimStr) {
+            if (this.notIncluded(aimStr)) {
+                return new ArrayList<>();
+            }
+            StringTokenizer st = new StringTokenizer(aimStr, this.val);
+            List<Long> result = new ArrayList<>();
+            while (st.hasMoreTokens()) {
+                String temp = st.nextToken();
+                result.add(Long.valueOf(temp));
+            }
+            return result;
+        }
+
+        public List<Integer> splitToInteger(String aimStr) {
+            if (this.notIncluded(aimStr)) {
+                return new ArrayList<>();
+            }
+            StringTokenizer st = new StringTokenizer(aimStr, this.val);
+            List<Integer> result = new ArrayList<>();
+            while (st.hasMoreTokens()) {
+                String temp = st.nextToken();
+                result.add(Integer.valueOf(temp));
+            }
+            return result;
+        }
+
+        public String join(Collection<String> srcList) {
+            return this.join(srcList, false);
+        }
+
+        public <T, R> String join(Collection<T> srcList, Function<? super T, ? extends R> function) {
+            return this.join(srcList, function, false);
+        }
+
+        public String join(Collection<String> srcList, boolean includeStartAndEnd) {
+            if (null == srcList || srcList.isEmpty()) {
+                return null;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (String str : srcList) {
+                sb.append(str);
+                sb.append(this.val);
+            }
+            String result = sb.toString();
+            result = result.substring(0, result.length() - this.val.length());
+            if (includeStartAndEnd) {
+                result = this.val + result + this.val;
+            }
+            return result;
+        }
+
+        public <T, R> String join(Collection<T> srcList, Function<? super T, ? extends R> function, boolean includeStartAndEnd) {
+            if (null == srcList || srcList.size() == 0) {
+                return null;
+            }
+            List<String> strList = srcList.stream().map(function).map(Object::toString).collect(Collectors.toList());
+            return this.join(strList, includeStartAndEnd);
+        }
     }
 
-    public static String nowDateStr(String pattern) {
-        return new SimpleDateFormat(pattern).format(new Date());
+    //正则表达式
+    public enum Regex {
+        ID_CARD("(^[0-9]{18}$)|(^[0-9]{17}(X|x)$)", "身份证号码"),
+        PHONE("(^(1)[0-9]{10}$)", "手机号"),
+        ;
+
+        private final String regexStr;
+        private final String desc;
+
+        Regex(String regexStr, String desc) {
+            this.regexStr = regexStr;
+            this.desc = desc;
+        }
+
+        public boolean verify(String aimStr) {
+            return null != aimStr && aimStr.length() != 0 && aimStr.matches(this.regexStr);
+        }
+
+        public boolean verifyFail(String aimStr) {
+            return !this.verify(aimStr);
+        }
     }
 
-    public static int monthDaysByDate(Date date) {
-        Calendar calendar = dateToCalendar(date);
-        calendar.set(Calendar.DATE, 1);
-        calendar.roll(Calendar.DATE, -1);
-        return calendar.get(Calendar.DATE);
+    /*****************************************以上为常量，以下为工具函数*****************************************/
+    public static LocalDateTime dayStart(LocalDateTime time) {
+        return time.withHour(0).withMinute(0).withSecond(0).withNano(0);
     }
 
-    public static Date dateStartTime(Date date) {
-        Calendar calendar = dateToCalendar(date);
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        return calendar.getTime();
+    public static LocalDateTime dayEnd(LocalDateTime time) {
+        return time.withHour(23).withMinute(59).withSecond(59).withNano(999999999);
     }
 
-    public static Date dateEndTime(Date date) {
-        Calendar calendar = dateToCalendar(date);
-        calendar.set(Calendar.HOUR_OF_DAY, 23);
-        calendar.set(Calendar.MINUTE, 59);
-        calendar.set(Calendar.SECOND, 59);
-        calendar.set(Calendar.MILLISECOND, 999);
-        return calendar.getTime();
+    public static LocalDateTime monthStart(LocalDateTime time) {
+        return dayStart(time.with(TemporalAdjusters.firstDayOfMonth()));
     }
 
-    public static boolean isEmpty(CharSequence cs) {
-        return cs == null || cs.length() == 0;
+    public static LocalDateTime monthEnd(LocalDateTime time) {
+        return dayEnd(time.with(TemporalAdjusters.lastDayOfMonth()));
+    }
+
+    public static LocalDateTime yearStart(LocalDateTime time) {
+        return dayStart(time.with(TemporalAdjusters.firstDayOfYear()));
+    }
+
+    public static LocalDateTime yearEnd(LocalDateTime time) {
+        return dayEnd(time.with(TemporalAdjusters.lastDayOfYear()));
+    }
+
+    public static String formatTime(LocalDateTime time, String datePattern) {
+        return DateTimeFormatter.ofPattern(datePattern).format(time);
+    }
+
+    public static String formatTime(LocalDateTime time, DatePattern datePattern) {
+        return DateTimeFormatter.ofPattern(datePattern.val()).format(time);
+    }
+
+    public static LocalDateTime parseTime(String timeStr, String datePattern) {
+        return LocalDateTime.parse(timeStr, DateTimeFormatter.ofPattern(datePattern));
+    }
+
+    public static LocalDateTime parseTime(String timeStr, DatePattern datePattern) {
+        return LocalDateTime.parse(timeStr, DateTimeFormatter.ofPattern(datePattern.val()));
+    }
+
+    public static LocalDateTime dateToLocal(Date date) {
+        return null != date ? date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime() : null;
+    }
+
+    public static Date localToDate(LocalDateTime localDateTime) {
+        return null != localDateTime ? Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()) : null;
+    }
+
+    public static double lngLatMeter(double srcLng, double srcLat, double aimLng, double aimLat) {
+        return EARTH_RADIUS * Math.acos(Math.cos(Math.toRadians(srcLat)) * Math.cos(Math.toRadians(aimLat))
+                * Math.cos(Math.toRadians(srcLng) - Math.toRadians(aimLng))
+                + Math.sin(Math.toRadians(srcLat)) * Math.sin(Math.toRadians(aimLat)));
     }
 
     public static boolean isEmpty(Collection<?> coll) {
         return coll == null || coll.isEmpty();
     }
 
-    public static boolean isNotEmpty(CharSequence cs) {
-        return !isEmpty(cs);
+    public static boolean isEmpty(CharSequence cs) {
+        return cs == null || cs.length() == 0;
     }
 
     public static boolean isNotEmpty(Collection<?> coll) {
         return !isEmpty(coll);
-    }
-
-    public static void isNotEmpty(CharSequence cs, Consumer<CharSequence> consumer) {
-        if (isEmpty(cs)) {
-            return;
-        }
-        consumer.accept(cs);
     }
 
     public static void isNotEmpty(Collection<?> coll, Consumer<Collection<?>> consumer) {
@@ -152,13 +283,82 @@ public final class DSUtil {
         consumer.accept(coll);
     }
 
-    public static <T> T randomGetOne(List<T> srcList) {
-        return srcList.get(ThreadLocalRandom.current().nextInt(srcList.size()));
+    public static boolean isNotEmpty(CharSequence cs) {
+        return !isEmpty(cs);
+    }
+
+    public static void isNotEmpty(String str, Consumer<String> consumer) {
+        if (isEmpty(str)) {
+            return;
+        }
+        consumer.accept(str);
+    }
+
+    public static List<Field> allFields(Class clazz) {
+        List<Field> result = new ArrayList<>();
+        do {
+            Field[] fields = clazz.getDeclaredFields();
+            if (null != fields && fields.length > 0) {
+                result.addAll(Arrays.asList(fields));
+            }
+            clazz = clazz.getSuperclass();
+        } while (null != clazz && !clazz.equals(Object.class));
+        return result;
+    }
+
+    public static <T, K> K copySomeFields(T src, K aim, String... fields) {
+        Map<String, Field> srcMap = allFields(src.getClass())
+                .stream().collect(Collectors.toMap(Field::getName, it -> it, (k1, k2) -> k1));
+        Map<String, Field> aimMap = allFields(aim.getClass())
+                .stream().collect(Collectors.toMap(Field::getName, it -> it, (k1, k2) -> k1));
+        List<String> fieldList = Arrays.asList(fields);
+        if (fieldList.size() == 0) {
+            fieldList = new ArrayList<>(srcMap.keySet());
+        }
+        for (String field : fieldList) {
+            Field srcField = srcMap.get(field);
+            Field aimField = aimMap.get(field);
+            if (null == srcField || null == aimField) {
+                continue;
+            }
+            srcField.setAccessible(true);
+            aimField.setAccessible(true);
+            try {
+                aimField.set(aim, srcField.get(src));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } finally {
+                srcField.setAccessible(false);
+                aimField.setAccessible(false);
+            }
+        }
+        return aim;
+    }
+
+    public static String randomNumberStrByLength(int length) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        while (count <= length) {
+            sb.append(randomGetOne(ZERO_TO_NINE));
+            count++;
+        }
+        return sb.toString();
+    }
+
+    public static String intToStrByLength(int num, Integer length) {
+        if (null == length) {
+            return num + "";
+        }
+        String str = num + "";
+        while (str.length() < length) {
+            str = "0".concat(str);
+        }
+        return str;
     }
 
     public static Double doubleFmt(Double value, Integer num) {
         if (value == null) {
-            return 0D;
+            return null;
         }
         if (num == null || num < 1) {
             return value;
@@ -171,20 +371,12 @@ public final class DSUtil {
         return Double.parseDouble(df.format(value));
     }
 
-    public static double lngLatDistance(double srcLng, double srcLat, double aimLng, double aimLat) {
-        return EARTH_RADIUS * Math.acos(Math.cos(Math.toRadians(srcLat)) * Math.cos(Math.toRadians(aimLat))
-                * Math.cos(Math.toRadians(srcLng) - Math.toRadians(aimLng))
-                + Math.sin(Math.toRadians(srcLat)) * Math.sin(Math.toRadians(aimLat)));
+    public static String get32UUID() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        return new UUID(random.nextLong(), random.nextLong()).toString().replace("-", "");
     }
 
-    public static String intToStrByLength(int num, Integer length) {
-        if (null == length) {
-            return num + "";
-        }
-        String str = num + "";
-        while (str.length() < length) {
-            str = "0".concat(str);
-        }
-        return str;
+    public static <T> T randomGetOne(List<T> srcList) {
+        return srcList.get(ThreadLocalRandom.current().nextInt(srcList.size()));
     }
 }
